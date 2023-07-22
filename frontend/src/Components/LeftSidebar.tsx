cad326ee070b720802fd586252213019068e9d99
SHARUN266 <sharunattari266@gmail.com>
2023-07-22 16:52:40 +0530

    Time-inbox done

--- frontend/src/Components/LeftSidebar.tsx
+++ frontend/src/Components/LeftSidebar.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from "react";
 import { BiHomeCircle, BiUser } from "react-icons/bi";
 import {
   BsBell,
@@ -39,40 +39,40 @@ const NAVIGATION_ITEMS = [
 function LeftSidebar() {
   return (
     <section className="fixed w-[275px] flex flex-col items-stretch h-screen  ">
-          <div className=" flex flex-col items-stretch h-full space-y-4 mt-4 ">
-            {NAVIGATION_ITEMS.map((item) => (
-              <Link
-                className="hover:bg-white/10 text-2xl transition duration-200 flex items-center justify-start w-fit space-x-2 rounded-3xl py-2 px-6"
-                href={`/${item.title.toLowerCase()}`}
-                key={item.title}
-              >
-                {" "}
-                <div>
-                  <item.icon />
-                </div>
-                {item.title !== "Twitter" && <div>{item.title}</div>}
-              </Link>
-            ))}
-
-            <button className="rounded-full m- bg-primary py-4  text-2xl text-center hover:bg-opacity-70  transition duration-2000000">
-              Tweet
-            </button>
-          </div>
-          <button className="rounded-full flex item-center space-x-2 m-4 p-4 hover:bg-white/10  transition duration-200  w-full justify-between">
-            <div  className="flex item-center space-x-2">
-              <div className="rounded-full bg-slate-400 w-12 h-12"></div>
-              <div className="text-left text-sm">
-                <div className="font-semibold">CoderSharun</div>
-                <div className="text-xs">@Sharun2666</div>
-              </div>
-            </div>
-
+      <div className=" flex flex-col items-stretch h-full space-y-4 mt-4 ">
+        {NAVIGATION_ITEMS.map((item) => (
+          <Link
+            className="hover:bg-white/10 text-2xl transition duration-200 flex items-center justify-start w-fit space-x-2 rounded-3xl py-2 px-6"
+            href={`/${item.title.toLowerCase()}`}
+            key={item.title}
+          >
+            {" "}
             <div>
-              <BsThreeDots />
+              <item.icon />
             </div>
-          </button>
-        </section>
-  )
+            {item.title !== "Twitter" && <div>{item.title}</div>}
+          </Link>
+        ))}
+
+        <button className="rounded-full m-4 bg-primary p-4  text-2xl text-center hover:bg-opacity-70  transition duration-2000000">
+          Tweet
+        </button>
+      </div>
+      <button className="rounded-full flex item-center space-x-2 p-4 hover:bg-white/10  transition duration-200  w-full justify-between">
+        <div className="flex item-center space-x-2">
+          <div className="rounded-full bg-slate-400 w-12 h-12"></div>
+          <div className="text-left text-sm">
+            <div className="font-semibold">CoderSharun</div>
+            <div className="text-xs">@Sharun2666</div>
+          </div>
+        </div>
+
+        <div>
+          <BsThreeDots />
+        </div>
+      </button>
+    </section>
+  );
 }
 
-export default LeftSidebar
+export default LeftSidebar;
